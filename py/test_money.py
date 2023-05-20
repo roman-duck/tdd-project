28c9eeda9227409835d4dfadc0654176cea3f27d
Red Rubber Duck <mvch@triangle.software>
2023-05-20 18:36:42 +0300

    green

--- py/test_money.py
+++ py/test_money.py
@@ -1,5 +1,12 @@
 import unittest
 
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount 
+
+    def times(self, multiplier):
+        return Dollar(10)
+
 class TestMoney(unittest.TestCase):
     def testMultiplication(self):
         fiver = Dollar(5)
