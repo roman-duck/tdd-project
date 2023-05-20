2d0e9ef50eae3f0191a7159f778bdb600fb025a8
Red Rubber Duck <mvch@triangle.software>
2023-05-20 22:50:38 +0300

    refactoring next step

--- go/money_test.go
+++ go/money_test.go
@@ -4,20 +4,33 @@ import (
     "testing"
 )
 
-func TestMultiplication(t *testing.T) {
-    fiver := Dollar{
-        amount: 5,
-    }
+func TestMultiplicationInDollars(t *testing.T) {
+    fiver := Money{amount: 5, currency: "USD"}
     tenner := fiver.Times(2)
     if tenner.amount != 10 {
         t.Errorf("Expected 10, got: [%d]", tenner.amount)
     }
+    if tenner.currency != "USD" {
+        t.Errorf("Expected UDS, got: [%s]", tenner.currency)
+    }
 }
 
-type Dollar struct {
+func TestMultiplicationInEuros(t *testing.T) {
+    tenEuros := Money{amount: 10, currency: "EUR"}
+    twentyEuros := tenEuros.Times(2)
+    if twentyEuros.amount != 20 {
+        t.Errorf("Expected 20, got: [%d]", twentyEuros.amount)
+    }
+    if twentyEuros.currency != "EUR" {
+        t.Errorf("Expected EUR, got: [%s]", twentyEuros.currency)
+    }
+}
+
+type Money struct {
     amount int
+    currency string
 }
 
-func (d Dollar) Times(multiplier int) Dollar {
-    return Dollar{amount: d.amount * multiplier}
+func (m Money) Times(multiplier int) Money {
+    return Money{amount: m.amount * multiplier, currency: m.currency}
 }
--- py/test_money.py
+++ py/test_money.py
@@ -1,17 +1,28 @@
 import unittest
 
-class Dollar:
-    def __init__(self, amount):
-        self.amount = amount 
+
+class Money:
+    def __init__(self, amount, currency):
+        self.amount = amount
+        self.currency = currency
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+
 
 class TestMoney(unittest.TestCase):
-    def testMultiplication(self):
-        fiver = Dollar(5)
+    def testMultiplicationInDollars(self):
+        fiver = Money(5, "USD")
         tenner = fiver.times(2)
         self.assertEqual(10, tenner.amount)
+        self.assertEqual("USD", tenner.currency)    
+
+    def testMultiplicationInEuros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = tenEuros.times(2)
+        self.assertEqual(20, twentyEuros.amount)
+        self.assertEqual("EUR", twentyEuros.currency)    
+
 
 if __name__ == '__main__':
     unittest.main()
