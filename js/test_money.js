b8f5e0fe7782f6ebd77a788b4fb0fab5ff908bc8
Red Rubber Duck <mvch@triangle.software>
2023-05-22 17:23:39 +0300

    conversion of Money from EUR to USD

--- js/test_money.js
+++ js/test_money.js
@@ -25,6 +25,15 @@ class MoneyTest {
     assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars)
   }
 
+  testAdditionDollarsAndEuros() {
+    let fiveDollars = new Money(5, "USD")
+    let tenEuros = new Money(10, "EUR")
+    let portfolio = new Portfolio();
+    portfolio.add(fiveDollars, tenEuros)
+    let expectedValue = new Money(17, "USD");
+    assert.deepStrictEqual(portfolio.evaluate("USD"), expectedValue)
+  }
+
   getAllTestMethods() {
     let moneyPrototype = MoneyTest.prototype;
     let allProps = Object.getOwnPropertyNames(moneyPrototype);
--- py/money.py
+++ py/money.py
@@ -9,6 +9,9 @@ class Money:
     def __eq__(self, other):
         return self.amount == other.amount and self.currency == other.currency
 
+    def __str__(self):
+        return f"{self.currency} {self.amount:0.2f}"
+
     def times(self, multiplier):
         return Money(self.amount * multiplier, self.currency)
 
